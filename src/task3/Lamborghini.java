8862a7a32ac225b0bc686b7719621e997b913fea
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-12 16:48:03 -0500

    Finalize task3 Startup class

--- src/task3/Lamborghini.java
+++ src/task3/Lamborghini.java
@@ -9,6 +9,44 @@ package task3;
  *
  * @author Isaac
  */
-public class Lamborghini {
-    
+public class Lamborghini extends SuperCar {
+    private String dealerName;
+    private String licensePlate;
+    private String vin;
+
+    public String getDealerName() {
+        return dealerName;
+    }
+
+    public void setDealerName(String dealerName) {
+        if(dealerName != null){
+            this.dealerName = dealerName;
+        } else {
+            throw new IllegalArgumentException("Name of dealer the car was purchased at is required");
+        }
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        if(licensePlate != null){
+            this.licensePlate = licensePlate;
+        } else {
+            throw new IllegalArgumentException("License plate is required");
+        }
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        if((vin != null) && ((vin.length() == 17 && this.getYear() >= 1981) || (vin.length() >= 11 && vin.length() <= 17 && this.getYear() < 1981))){
+            this.vin = vin;
+        } else {
+            throw new IllegalArgumentException("VIN is required");
+        }
+    }
 }
