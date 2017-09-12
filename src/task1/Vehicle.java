8f51703826e450129291d0ceb4254c268dcbd8b0
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-12 15:34:57 -0500

    Properties and methods in Vehicle class

--- src/task1/Vehicle.java
+++ src/task1/Vehicle.java
@@ -10,5 +10,46 @@ package task1;
  * @author Isaac
  */
 public class Vehicle {
+    private String manufacturer;
+    private boolean registrationRequired;
+    private boolean running;
+    private boolean off;
+    
+    public String getPowerSource(){
+        return "motor";
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isOff() {
+        return off;
+    }
+
+    public void setOff(boolean off) {
+        this.off = off;
+    }
+    
     
 }
