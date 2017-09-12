69607d5de8d595aeb0f5397e44d1aeaa7553192d
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-12 16:43:15 -0500

    Vehicle interface in task3

--- src/task3/Vehicle.java
+++ src/task3/Vehicle.java
@@ -10,5 +10,17 @@ package task3;
  * @author Isaac
  */
 public interface Vehicle {
-    
+    public abstract double get0To60Time();
+
+    public abstract String getManufacturer();
+
+    public abstract void setManufacturer(String manufacturer);
+
+    public abstract boolean isRunning();
+
+    public abstract void setRunning(boolean running);
+
+    public abstract boolean isOff();
+
+    public abstract void setOff(boolean off);
 }
