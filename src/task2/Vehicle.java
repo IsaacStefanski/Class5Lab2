9db304d5dfc29dde573c7468d213d320315152af
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-12 16:34:43 -0500

    Work in Vehicle class in task2

--- src/task2/Vehicle.java
+++ src/task2/Vehicle.java
@@ -10,5 +10,37 @@ package task2;
  * @author Isaac
  */
 public abstract class Vehicle {
+    private String manufacturer;
+    private boolean running;
+    private boolean off;
     
+    public abstract double get0To60Time();
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        if(manufacturer != null){
+            this.manufacturer = manufacturer;
+        } else {
+            throw new IllegalArgumentException("Manufacturer is required");
+        }
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isOff() {
+        return off;
+    }
+
+    public void setOff(boolean off) {
+        this.off = off;
+    }
 }
