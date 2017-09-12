d4f17425ad4888724feeabd51b23fa04faae4834
Isaac <Isaac@ISAAC_HP_OMEN>
2017-09-12 11:08:20 -0500

    Create Class5Lab2 project with 3 packages

---
+++ src/task1/Class5Lab2.java
@@ -0,0 +1,16 @@
+package task1;
+
+/**
+ *
+ * @author Isaac
+ */
+public class Class5Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
